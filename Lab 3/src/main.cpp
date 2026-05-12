e216da9f1ece7e7831cd2437a01ee8146b305a36
Ishan Amalsha <ishanamalsha934@gmail.com>
2026-05-12 11:50:11 +0530

    Lab 3

--- Lab 3/src/main.cpp
+++ Lab 3/src/main.cpp
@@ -1,18 +1,19 @@
-#include <Arduino.h>
+#include <avr/io.h>
+int main(void)
+{
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ DDRB |= (1<<0);
+ DDRD &= ~(1<<2);
+ PORTD |= (1<<2);
+ while (1)
+ {
+ if (PIND & (1<<2))
+ {
+ PORTB |= (1<<0);
+ }
+ else
+ {
+ PORTB &= ~(1<<0);
+ }
+ }
 }
